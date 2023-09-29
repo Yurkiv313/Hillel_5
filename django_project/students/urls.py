76ae283e698d6e91cc9486fccd07de0228291ece
Andriy Yurkiv <andriy.yurkiv@gmail.com>
2023-09-29 15:28:55 +0300

    Add black

--- django_project/students/urls.py
+++ django_project/students/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('generate-student/', views.one_user, name='one_user'),
-    path('generate-students/', views.students_generate, name='students_generate')
+    path("generate-student/", views.one_user, name="one_user"),
+    path("generate-students/", views.students_generate, name="students_generate"),
 ]
--- django_project/students/views.py
+++ django_project/students/views.py
@@ -11,21 +11,19 @@ def one_user(request):
     min_date = datetime.date(2000, 1, 1)
     max_date = datetime.date(2006, 12, 31)
     student = {
-        'first_name': fake.first_name(),
-        'last_name': fake.last_name(),
-        'birth': fake.date_between_dates(date_start=min_date, date_end=max_date),
-        'phone': fake.phone_number(),
-        'address': fake.address()
+        "first_name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "birth": fake.date_between_dates(date_start=min_date, date_end=max_date),
+        "phone": fake.phone_number(),
+        "address": fake.address(),
     }
     x.append(student)
-    return render(request, "students_generate/student.html", {
-        'students': x
-    })
+    return render(request, "students_generate/student.html", {"students": x})
 
 
 def students_generate(request):
     queryprms = request.GET
-    count = queryprms.get('count', '1')
+    count = queryprms.get("count", "1")
     if not count.isdigit() or (int(count) < 0 or int(count) > 100):
         return HttpResponse("Введіть додатнє число не більше 100")
     fake = Faker()
@@ -35,13 +33,13 @@ def students_generate(request):
     max_date = datetime.date(2006, 12, 31)
     for _ in range(c):
         user = {
-            'first_name': fake.first_name(),
-            'last_name': fake.last_name(),
-            'birth': fake.date_between_dates(date_start=min_date, date_end=max_date),
-            'phone': fake.phone_number(),
-            'address': fake.address()
+            "first_name": fake.first_name(),
+            "last_name": fake.last_name(),
+            "birth": fake.date_between_dates(date_start=min_date, date_end=max_date),
+            "phone": fake.phone_number(),
+            "address": fake.address(),
         }
         x.append(user)
-    return render(request, "students_generate/student.html", {
-        'students': x
-    })
+    return render(request, "students_generate/student.html", {"students": x})
+
+#quantity = int_quantity if int_quantity > 0 else 100
