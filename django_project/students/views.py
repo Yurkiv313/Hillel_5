47835be36a6f488f3e713241f11880b71c3e9ed9
Andriy Yurkiv <andriy.yurkiv@gmail.com>
2023-10-03 13:36:08 +0300

    correction

--- django_project/students/views.py
+++ django_project/students/views.py
@@ -43,5 +43,3 @@ def students_generate(request):
         x.append(user)
     return render(request, "students_generate/student.html", {"students": x})
 
-
-# quantity = int_quantity if int_quantity > 0 else 100
